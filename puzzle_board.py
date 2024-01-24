639426cf8df06bbea368268d5812ff9150caaefe
Cale <caledillon@cmail.carleton.ca>
2024-01-24 11:45:33 -0500

    Fixed tile movement bug caused by the use of one-dimensional arrays for storing tiles

--- puzzle_board.py
+++ puzzle_board.py
@@ -1,5 +1,5 @@
 from pygame import Surface
-from random import randint
+from random import choice
 
 
 class PuzzleBoard:
@@ -22,19 +22,11 @@ class PuzzleBoard:
         self.tiles[-1] = None
 
     def __shuffle_tiles(self, shuffle_moves: int):
-        empty_pos = len(self.tiles) - 1
         for _ in range(shuffle_moves):
-            adjacent_positions = []
-            if empty_pos + 1 < len(self.tiles):
-                adjacent_positions.append(1)
-            if empty_pos - 1 >= 0:
-                adjacent_positions.append(-1)
-            if empty_pos + self.DIMENSION < len(self.tiles):
-                adjacent_positions.append(self.DIMENSION)
-            if empty_pos - self.DIMENSION >= 0:
-                adjacent_positions.append(-self.DIMENSION)
-            empty_pos += adjacent_positions[randint(0, len(adjacent_positions) - 1)]
-            self.__move_tile(empty_pos)
+            empty_pos = self.tiles.index(None)
+            self.__move_tile(
+                empty_pos + choice((1, -1, self.DIMENSION, -self.DIMENSION))
+            )
 
     def __move_tile(self, tile_position: int):
         if self.__valid_move(tile_position):
@@ -42,10 +34,18 @@ class PuzzleBoard:
             self.tiles[tile_position] = None
 
     def __valid_move(self, tile_position: int) -> bool:
-        if tile_position + 1 < len(self.tiles):
+        if tile_position not in range(len(self.tiles)):
+            return False
+        if (
+            tile_position + 1 < len(self.tiles)
+            and (tile_position + 1) % self.DIMENSION != 0
+        ):
             if not self.tiles[tile_position + 1]:
                 return True
-        if tile_position - 1 >= 0:
+        if (
+            tile_position - 1 >= 0
+            and (tile_position - 1) % self.DIMENSION != self.DIMENSION - 1
+        ):
             if not self.tiles[tile_position - 1]:
                 return True
         if tile_position + self.DIMENSION < len(self.tiles):
